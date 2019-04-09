0e84a6f3843be484ae1c8a1f6798f676294bf792
vincelep <vincent.leparoux@gmail.com>
2019-04-09 16:12:59 +0200

    add some tests to items_spec models

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,6 +15,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.create(:item)
+  end
+
+  it "has a valid factory" do
+    expect(build(:item)).to be_valid
+  end
+
   describe 'Model instantiation' do
     subject(:new_item) { described_class.new }
 
@@ -22,6 +30,31 @@ RSpec.describe Item, type: :model do
       it { is_expected.to have_db_column(:id).of_type(:integer) }
       it { is_expected.to have_db_column(:original_price).of_type(:float).with_options(null: false) }
       it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:has_discount).of_type(:boolean).with_options(default: false) }
+      it { is_expected.to have_db_column(:discount_percentage).of_type(:integer).with_options(null: true) }
+    end
+  end
+
+  context "with validation attributes" do
+    let(:item) { build(:item) }
+
+    it "is valid with valid attributes" do
+      expect(item).to be_a(Item)
+    end
+
+    describe "original_price" do
+      it { expect(item).to validate_presence_of(:original_price) }
+      it { expect(item).to validate_numericality_of(:original_price).is_greater_than(0) }
+    end
+
+    describe "has_discount" do
+      it { is_expected.not_to allow_value(nil).for(:has_discount) }
+    end
+
+    describe "discount_percentage" do
+      it { expect(item).to validate_numericality_of(:discount_percentage).only_integer }
+      it { is_expected.to validate_inclusion_of(:discount_percentage).in_range(1..99) }
     end
   end
 
@@ -30,6 +63,13 @@ RSpec.describe Item, type: :model do
       let(:item) { build(:item_with_discount, original_price: 100.00, discount_percentage: 20) }
 
       it { expect(item.price).to eq(80.00) }
+    end
+
+    context 'when the item has no discount' do
+      let(:item) { build(:item_without_discount, original_price: 50.00) }
+
+      it { expect(item.price).to eq(50.00) }
+      it { expect(item.has_discount).to be(false) }
     end
   end
 end
