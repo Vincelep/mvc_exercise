cb39fc089b016d8ac93670ffda7b1c662429c5a2
vincelep <vincent.leparoux@gmail.com>
2019-04-10 13:29:44 +0200

    add flash succes to update method

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -10,6 +10,7 @@ module Administration
     def update
       @item = Item.find(params[:id])
       @item.update(has_discount: true, discount_percentage: params[:item]['discount_percentage']) if params[:item]['discount_percentage'] != 0
+      flash[:notice] = "L'item a bien été modifié."
       redirect_to administration_items_path
     end
   end
